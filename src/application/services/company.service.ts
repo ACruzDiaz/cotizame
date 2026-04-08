de6fc7424007daeb919f735cf8988199641f6caf
alan cruz <cruza86@uabc.edu.mx>
2026-04-08 15:48:03 -0700

    service company

---
+++ src/application/services/company.service.ts
@@ -0,0 +1,97 @@
+import { prisma } from "../connection/prismaClient";
+import type {
+  Prisma,
+  Company,
+  CompanySecret,
+  CompanyTier,
+} from "../../generated/prisma/client";
+
+//Create types for custom payload response from DB
+type CompanyCreatePayload = Prisma.CompanySecretGetPayload<{
+  select: {
+    companyId:true
+  };
+}>;
+
+class CompanyService {
+  constructor() {}
+
+  public async create(
+    phoneNumber: string,
+    name: string,
+    createdAt: Date,
+    updatedAt: Date,
+  ): Promise<Company> {
+    try {
+      return await prisma.company.create({
+        data: {
+          phoneNumber,
+          name,
+          createdAt,
+          updatedAt,
+        },
+      });
+    } catch (error) {
+      throw new Error(`Failed to create a company. Details: ${error}`);
+    }
+  }
+
+  public async assignSecret(
+    companyId: string,
+    whatsappTokenHash: string,
+    phoneNumberId: string,
+  ): Promise<CompanyCreatePayload> {
+    try {
+      return await prisma.companySecret.upsert({
+        where: { companyId },
+        update: {
+          whatsappTokenHash,
+          phoneNumberId,
+        },
+        create: {
+          companyId,
+          whatsappTokenHash,
+          phoneNumberId,
+        },
+        select:{
+          companyId: true,
+        }
+      });
+    } catch (error) {
+      throw new Error(`Failed to assign secret. Details: ${error}`);
+    }
+  }
+
+  public async getByPhoneNumber(phoneNumber: string): Promise<Company | null> {
+    try {
+      return await prisma.company.findUnique({
+        where: { phoneNumber },
+      });
+    } catch (error) {
+      throw new Error(
+        `Failed to get company by phone number. Details: ${error}`,
+      );
+    }
+  }
+
+  public async getById(id: string): Promise<Company | null> {
+    try {
+      return await prisma.company.findUnique({
+        where: { id },
+      });
+    } catch (error) {
+      throw new Error(`Failed to get company by id. Details: ${error}`);
+    }
+  }
+
+  public async updateTier(id: string, tier: CompanyTier): Promise<Company> {
+    try {
+      return await prisma.company.update({
+        where: { id },
+        data: { tier },
+      });
+    } catch (error) {
+      throw new Error(`Failed to update company tier. Details: ${error}`);
+    }
+  }
+}
