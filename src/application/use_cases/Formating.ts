6e053dc8b504faa864801b4f248d2abdd368e4b2
alan cruz <cruza86@uabc.edu.mx>
2026-04-13 00:35:47 -0700

    Formating class for entities products and company

---
+++ src/application/use_cases/Formating.ts
@@ -0,0 +1,43 @@
+import type { Client, Company, Product } from "../../generated/prisma/client";
+
+interface FormatEntity {
+  productsList: Product[];
+  company: Company | null;
+}
+
+export class Formating {
+  private concatResult: string = "";
+
+  private productsList: Product[];
+  private company: Company | null;
+
+  constructor(formatEntity: FormatEntity) {
+    this.productsList = formatEntity.productsList;
+    this.company = formatEntity.company;
+  }
+
+  public formatProductList(): this {
+    const parsedProductList = this.productsList
+      .map((t) => `- ${t.name}: ${t.description}.`)
+      .join("\n");
+
+    this.concatResult += parsedProductList;
+    return this;
+  }
+
+  public formatCompany(): this {
+    if (this.company?.website) {
+      this.concatResult += `\n${this.company.website}`;
+    }
+    return this;
+  }
+
+  public customMessage(message:string): this {
+    this.concatResult += message
+    return this
+  }
+
+  public build(): string {
+    return this.concatResult;
+  }
+}
